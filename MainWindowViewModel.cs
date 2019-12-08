c90e2ec97af5037a9905c9fee096d8b56594e555
Abhishek Pandey <pandeyabhishek28@outlook.com>
2019-12-08 14:00:13 +0530

    Sort an Array
    Sort an Array by using Merge sort

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,87 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SortAnArray
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _inputData;
+        public string InputData
+        {
+            get { return _inputData; }
+            set
+            {
+                _inputData = value;
+                RaisePropertyChanged("InputData");
+            }
+        }
+
+        private string _outputData;
+        public string OutputData
+        {
+            get { return _outputData; }
+            set
+            {
+                _outputData = value;
+                RaisePropertyChanged("OutputData");
+            }
+        }
+
+        public RelayCommand SortInputArrayCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            SortInputArrayCommand = new RelayCommand(SortInputData);
+        }
+
+        private void SortInputData()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(InputData))
+                {
+                    OutputData = string.Empty;
+                    return;
+                }
+                if (!InputData.Contains(","))
+                {
+                    if (double.TryParse(InputData, out double output))
+                    {
+                        OutputData = output.ToString();
+                        return;
+                    }
+                    OutputData = string.Empty;
+                    return;
+                }
+
+                var inputData = new List<double>();
+                foreach (var input in InputData.Split(',').Where(x => !string.IsNullOrEmpty(x)))
+                {
+                    if (double.TryParse(input, out double parshedInput))
+                    {
+                        inputData.Add(parshedInput);
+                    }
+                }
+
+                var inputArray = inputData.ToArray();
+
+                inputArray.MergeSort();
+
+                OutputData = string.Join(",", inputArray);
+            }
+            catch(Exception ex)
+            {
+                // this is wrong
+                // we cann't show a dialog from view model
+                // as no logging is implemented so I am adding it here
+                MessageBox.Show(ex.Message, "Error!");
+            }
+        }
+    }
+}
---
+++ Sorting.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAnArray
+{
+    static class Sorting
+    {
+        public static void MergeSort<T>(this T[] input)
+        {
+            MergeSort<T>(input, Comparer<T>.Default);
+        }
+
+        public static void MergeSort<T>(this T[] input, IComparer<T> Comparer)
+        {
+            MergeSort<T>(input, Comparer.Compare);
+        }
+
+        public static void MergeSort<T>(this T[] input, Comparison<T> Comparison)
+        {
+            if (input.Length <= 1) return;
+
+            T[] left = new T[(input.Length + 1) / 2];
+            T[] right = new T[input.Length - left.Length];
+
+            Array.Copy(input, left, left.Length);
+
+            for (int i = left.Length, j = 0; i < input.Length; i++, j++)
+            {
+                right[j] = input[i];
+            }
+            
+            left.MergeSort(Comparison);
+            right.MergeSort(Comparison);
+
+            T[] temp = Merge(left, right, Comparison);
+            temp.CopyTo(input, 0);
+        }
+
+        private static T[] Merge<T>(T[] Left, T[] Right, Comparison<T> Comparison)
+        {
+            T[] ret = new T[Left.Length + Right.Length];
+            var index = 0;
+            var leftCount = Left.Count();
+            var rightCount = Right.Count();
+            int i = 0, j = 0;
+
+            while (i < leftCount && j < rightCount)
+            {
+                if (Comparison(Left[i], Right[j]) <= 0)
+                {
+                    ret[index] = Left[i];
+                    i++;
+                }
+                else
+                {
+                    ret[index] = Right[j];
+                    j++;
+                }
+                index++;
+            }
+            while (i < Left.Length)
+            {
+                ret[index] = Left[i];
+                i++;
+                index++;
+            }
+
+            while (j < Right.Length)
+            {
+                ret[index] = Right[j];
+                j++;
+                index++;
+            }
+            return ret;
+        }
+    }
+}
